328db1098f9ea0d19ff9d6497268ba83872062f6
Lorean <lorean.carlos@gmail.com>
2025-04-18 20:34:37 -0300

    fixed backend talked Session count

--- src/controllers/callModeSession.controller.js
+++ src/controllers/callModeSession.controller.js
@@ -8,11 +8,9 @@ export class CallModeSessionController {
    create = async (req, res) => {
       try {
          const userId = req.user.id;
-         console.log(req.body);
          const session = await this.callModeSessionService.create({
             ...req.body, userId
          });
-         console.log(session);
          return res.status(201).json(session);
       } catch (error) {
          return res.status(500).json({ error: 'Erro interno do servidor' });
